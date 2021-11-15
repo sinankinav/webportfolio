7ca8ea4a5fa20ba47ff98bb442db8e8def710011
sinankinav <78664500+sinankinav@users.noreply.github.com>
2021-11-16 01:26:32 +0300

    Update
    new comment rows and refresh button added

--- js/index.js
+++ js/index.js
@@ -1,19 +1,45 @@
-$(document).ready(function(){
+var inputText,comment,btnSil,imgCol,liste,tr;
 
-$('#comment').click(function(){
-var input = $('#input').val();
-$('.box').append(input + '<br>');
-$('#input').val('')
-$('.box-container').slideDown();
-});
+function ekle(){
+    inputText =document.querySelector("#input");
+    liste = document.querySelector("#liste");
+    if (inputText.value ==="") {
+        return;
+    }
+   
+    comment = document.createElement("td");
+    imgCol =document.createElement("td")
 
-$('#cancel').click(function(){
-    $('#input').val('');
-});
+    comment.setAttribute("id","comment");
 
-$('#delete').click(function(){
-    $('.box').text('');
-    $('.box-container').slideUp();
-});
+    imgCol.setAttribute("type","button");
+    imgCol.setAttribute("id","btnSil");
+    imgCol.setAttribute("onClick","satirSil(this)");
 
-});
+    comment.textContent = inputText.value;
+    imgCol.innerHTML = '<a  class="delete-item float-right"><i class="fas fa-times"></i></a>'
+
+    tr = document.createElement("tr");
+    tr.appendChild(comment);
+    tr.appendChild(imgCol);
+    
+    liste.appendChild(tr);
+    
+    inputText.value = "";
+    inputText.focus();
+    
+}
+
+function temizle(){
+    if (liste != null) {
+        var rowCount = liste.rows.length;
+        for (var i = 0; i < rowCount; i++){
+            liste.deleteRow(0);
+        }
+    }
+}
+function satirSil(e){
+    
+    e.closest('tr').remove();
+   
+}
